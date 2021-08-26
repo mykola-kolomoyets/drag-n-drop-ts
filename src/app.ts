1a5e7bd1ec5fb8103bfe2e91c0b61a28c34ff87f
Kolomoyets473 <kolomoyets473@gmail.com>
2021-08-26 22:27:16 +0300

    v0.1.0
    === NEW ===
    1) added templates for elements of application
    2) created basic class to display template
    3) added bs-config.json to configure watching the files with lite-server

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+class ProjectInput {
+	public templateElement: HTMLTemplateElement;
+	public hostElement: HTMLDivElement;
+	public element: HTMLFormElement;
+
+	constructor() {
+		this.templateElement = document.getElementById('project-input') as HTMLTemplateElement;
+		this.hostElement = document.getElementById('app') as HTMLDivElement;
+
+		const importedNode = document.importNode(this.templateElement.content, true);
+		this.element = importedNode.firstElementChild as HTMLFormElement;
+
+		this.attach();
+	}
+
+	private attach() { 
+		this.hostElement.insertAdjacentElement('afterbegin', this.element);
+	}
+}
+
+const projInput = new ProjectInput();
