292de8343661580108c68476c982ec981265a7f9
Kolomoyets473 <kolomoyets473@gmail.com>
2021-08-26 22:39:28 +0300

    v0.1.1
    === NEW ===
    1) added submit handler for form

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,41 @@
 class ProjectInput {
+	// DOM elements
 	public templateElement: HTMLTemplateElement;
 	public hostElement: HTMLDivElement;
 	public element: HTMLFormElement;
+	public titleInputElement: HTMLInputElement;
+	public descriptionInputElement: HTMLInputElement;
+	public peopleInputElement: HTMLInputElement;
 
 	constructor() {
 		this.templateElement = document.getElementById('project-input') as HTMLTemplateElement;
 		this.hostElement = document.getElementById('app') as HTMLDivElement;
 
 		const importedNode = document.importNode(this.templateElement.content, true);
-		this.element = importedNode.firstElementChild as HTMLFormElement;
 
+		this.element = importedNode.firstElementChild as HTMLFormElement;
+		this.element.id = 'user-input';
+
+		this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+		this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+		this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+		this.configure();
 		this.attach();
 	}
 
 	private attach() { 
 		this.hostElement.insertAdjacentElement('afterbegin', this.element);
+	}
+
+	private submitHandler(event: Event) {
+		event.preventDefault();
+
+		console.log(this.titleInputElement.value);
+	};
+
+	private configure() {
+		this.element.addEventListener('submit', this.submitHandler.bind(this));
 	}
 }
 
